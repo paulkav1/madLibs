c43282f27515620f092cb26e2aa9dc1f8f8ef4de
Paul Kavanagh <paulkav1@gmail.com>
2015-02-21 14:47:19 -0500

    a

--- madlib.js
+++ madlib.js
@@ -1,5 +1,14 @@
 angular.module('madApp', [])
-.controller('myC', function() {
-	this.female_name = "Jill";
-	this.adjective = 'tiresome';
+.controller('myC', function($scope) {
+	$scope.myC = {};
+	$scope.myC.jerk = "Gibbles";
+	$scope.myC.dirty_task = "dog walking";
+	$scope.myC.celebrity = "Oprah";	
+
+	$scope.appState = 1;	
+	//the following code is not working
+	if (myForm.$invalid) 
+		$scope.myC.message = "error!";	
+	else
+		$scope.myC.message = "OK I guess";
 });
