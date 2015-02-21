09a49ef1c90673973c3a85c6e9ebaedd4f077ea2
Paul Kavanagh <paulkav1@gmail.com>
2015-02-21 14:48:58 -0500

    Update madlib.js

--- madlib.js
+++ madlib.js
@@ -1,4 +1,14 @@
-angular.module('madLibs', [])
-.controller('paulCtrl', function(VERSION, $scope) {
-	$scope.version = VERSION;
+angular.module('madApp', [])
+.controller('myC', function($scope) {
+	$scope.myC = {};
+	$scope.myC.jerk = "Gibbles";
+	$scope.myC.dirty_task = "dog walking";
+	$scope.myC.celebrity = "Oprah";	
+
+	$scope.appState = 1;	
+	//the following code is not working
+	if (myForm.$invalid) 
+		$scope.myC.message = "error!";	
+	else
+		$scope.myC.message = "OK I guess";
 });
